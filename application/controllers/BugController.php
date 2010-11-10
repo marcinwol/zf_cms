cc2be20cd266737c08b2fa7dd84b17b6d031e3b9
marcinwol <marcin@y7mail.com>
2010-11-10 13:51:13 +0800

    Added Bug adding form along with validation.

--- application/controllers/BugController.php
+++ application/controllers/BugController.php
@@ -1,25 +1,32 @@
 <?php
 
-class BugController extends Zend_Controller_Action
-{
+class BugController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
 
-    public function createAction()
-    {
-        // action body
+    public function submitAction() {
+
+        $formBugReport = new My_Form_BugReportForm();
+        $formBugReport->setAction($this->view->baseUrl() . '/bug/submit/');
+        $formBugReport->setMethod('post');
+
+        if ($this->getRequest()->isPost()) {
+            if ($formBugReport->isValid($_POST)) {
+                print_r($formBugReport->getValues());
+            }
+        }
+        $this->view->form = $formBugReport;
     }
 
+    public function createAction() {
+        // action body
+    }
 
 }
-
-
 
--- application/forms/BugReportForm.php
+++ application/forms/BugReportForm.php
@@ -11,9 +11,11 @@
  * @author marcin
  */
 class My_Form_BugReportForm extends Zend_Form {
-
     //put your code here
 
+    /**
+     * @todo Finish BugReportForm
+     */
     public function init() {
         // add element: author textbox
         $author = $this->createElement('text', 'author');
@@ -21,6 +23,67 @@ class My_Form_BugReportForm extends Zend_Form {
         $author->setRequired(TRUE);
         $author->setAttrib('size', 30);
         $this->addElement($author);
+
+        // add element: email textbox 
+        $email = $this->createElement('text', 'email');
+        $email->setLabel('Your email address:');
+        $email->setRequired(TRUE);
+        $email->addValidator(new Zend_Validate_EmailAddress());
+        $email->addFilters(array(
+            new Zend_Filter_StringTrim(),
+            new Zend_Filter_StringToLower()
+        ));
+        $email->setAttrib('size', 40);
+        $this->addElement($email);
+
+
+        // add element: date textbox
+        $date = $this->createElement('text', 'date');
+        $date->setLabel('Date the issue occurred (mm-dd-yyyy):');
+        $date->setRequired(TRUE);
+        $date->addValidator(new Zend_Validate_Date('MM-DD-YYYY'));
+        $date->setAttrib('size', 20);
+        $this->addElement($date);
+
+
+        // add element: URL textbox
+        $url = $this->createElement('text', 'url');
+        $url->setLabel('Issue URL:');
+        $url->setRequired(TRUE);
+        $url->setAttrib('size', 50);
+        $this->addElement($url);
+
+        // add element: description text area
+        $description = $this->createElement('textarea', 'description');
+        $description->setLabel('Issue description:');
+        $description->setRequired(TRUE);
+        $description->setAttrib('cols', 50);
+        $description->setAttrib('rows', 4);
+        $this->addElement($description);
+
+        // add element: priority select box
+        $priority = $this->createElement('select', 'priority');
+        $priority->setLabel('Issue priority:');
+        $priority->setRequired(TRUE);
+        $priority->addMultiOptions(array(
+            'low' => 'Low',
+            'med' => 'Medium',
+            'high' => 'High'
+        ));
+        $this->addElement($priority);
+
+        // add element: status select box
+        $status = $this->createElement('select', 'status');
+        $status->setLabel('Current status:');
+        $status->setRequired(TRUE);
+        $status->addMultiOption('new', 'New');
+        $status->addMultiOption('in_progress', 'In Progress');
+        $status->addMultiOption('resolved', 'Resolved');
+        $this->addElement($status);
+
+
+        //add element: submit button
+        $this->addElement('submit', 'submit', array('label' => 'Submit'));
     }
 
 }
