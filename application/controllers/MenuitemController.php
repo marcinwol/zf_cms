da463fa1ab4b5ae9b4219a769019ea439a194878
marcin <marcin@marcin-mint.(none)>
2010-12-26 12:44:39 +0800

    Finished managment of menues,

--- application/controllers/MenuitemController.php
+++ application/controllers/MenuitemController.php
@@ -37,18 +37,75 @@ class MenuitemController extends Zend_Controller_Action {
                 $data = $frmMenuItem->getValues();
                 $mdlMenuItem = new My_Model_MenuItem();
                 $result = $mdlMenuItem->addItem($data['menu_id'], $data['label'],
-                                      $data['page_id'], $data['link']);
+                                $data['page_id'], $data['link']);
                 if ($result) {
                     $this->_request->setParam('menu', $data['menu_id']);
                     $this->_forward('index');
                     return;
-                } 
+                }
             }
         }
 
 
         $frmMenuItem->populate(array('menu_id' => $menuID));
         $this->view->form = $frmMenuItem;
+    }
+
+    public function moveAction() {
+        $id = $this->_request->getParam('id', null);
+
+        if (is_null($menuID)) {
+            return $this->_redirect('/menu/index');
+        }
+
+        $direction = $this->_request->getParam('direction');
+
+        $mdlMenuItem = new My_Model_MenuItem ( );
+        $menuItem = $mdlMenuItem->find($id)->current();
+        if ($direction == 'up') {
+            $mdlMenuItem->moveUp($id);
+        } elseif ($direction == 'down') {
+            $mdlMenuItem->moveDown($id);
+        }
+        $this->_request->setParam('menu', $menuItem->menu_id);
+        $this->_forward('index');
+    }
+
+    public function updateAction() {
+
+
+        $id = $this->_request->getParam('id');
+        // fetch the current item
+        $mdlMenuItem = new My_Model_MenuItem();
+        $currentMenuItem = $mdlMenuItem->find($id)->current();
+        // fetch its menu
+        $mdlMenu = new My_Model_Menu();
+        $this->view->menu = $mdlMenu->find($currentMenuItem->menu_id)->current();
+        // create and populate the form instance
+        $frmMenuItem = new My_Form_MenuItem();
+        $frmMenuItem->setAction($this->view->baseUrl() . '/menuitem/update');
+        // process the postback
+        if ($this->_request->isPost()) {
+            if ($frmMenuItem->isValid($_POST)) {
+                $data = $frmMenuItem->getValues();
+                $mdlMenuItem->updateItem($data['id'], $data['label'],
+                        $data['page_id'], $data['link']);
+                $this->_request->setParam('menu', $data['menu_id']);
+                return $this->_forward('index');
+            }
+        } else {
+            $frmMenuItem->populate($currentMenuItem->toArray());
+        }
+        $this->view->form = $frmMenuItem;
+    }
+
+    public function deleteAction() {
+        $id = $this->_request->getParam('id');
+        $mdlMenuItem = new My_Model_MenuItem ( );
+        $currentMenuItem = $mdlMenuItem->find($id)->current();
+        $mdlMenuItem->deleteItem($id);
+        $this->_request->setParam('menu', $currentMenuItem->menu_id);
+        $this->_forward('index');
     }
 
 }
--- application/models/MenuItem.php
+++ application/models/MenuItem.php
@@ -28,7 +28,7 @@ class My_Model_MenuItem extends Zend_Db_Table_Abstract {
 
     public function getItemsByMenu($menuId) {
 
-     
+
         $select = $this->select();
         $select->where("menu_id = ?", $menuId);
         $select->order("position");
@@ -40,14 +40,114 @@ class My_Model_MenuItem extends Zend_Db_Table_Abstract {
         }
     }
 
-    public function addItem($menu_id,$label, $page_id, $link) {
+    public function addItem($menu_id, $label, $page_id, $link) {
         $row = $this->createRow(array(
-            'menu_id' => $menu_id,
-            'label' => $label,
-            'page_id' => $page_id,
-            'link' => $link
-        ));
+                    'menu_id' => $menu_id,
+                    'label' => $label,
+                    'page_id' => $page_id,
+                    'link' => $link
+                ));
         return $row->save();
+    }
+
+    /**
+     * Get last menuitem position.
+     *
+     * @param int $menuID
+     * @return int menu position number
+     */
+    private function _getLastPosition($menuID) {
+        $select = $this->select();
+        $select->where("menu_id = ?", $menuID);
+        $select->order('position DESC');
+        $row = $this->fetchAll($select);
+
+        if ($row) {
+            return $this->position;
+        } else {
+            return 0;
+        }
+    }
+
+    public function moveUp($itemId) {
+        $row = $this->find($itemId)->current();
+        if ($row) {
+            $position = $row->position;
+            if ($position < 1) {
+                // this is already the first item
+                return false;
+            } else {
+                //find the previous item
+                $select = $this->select();
+                $select->order('position DESC');
+                $select->where("position < ?", $position);
+                $select->where("menu_id = ?", $row->menu_id);
+                $previousItem = $this->fetchRow($select);
+                if ($previousItem) {
+                    //switch positions with the previous item
+                    $previousPosition = $previousItem->position;
+                    $previousItem->position = $position;
+                    $previousItem->save();
+                    $row->position = $previousPosition;
+                    $row->save();
+                }
+            }
+        } else {
+            throw new Zend_Exception("Error loading menu item");
+        }
+    }
+
+    public function moveDown($itemId) {
+        $row = $this->find($itemId)->current();
+        if ($row) {
+            $position = $row->position;
+            if ($position == $this->_getLastPosition($row->menu_id)) {
+                // this is already the last item
+                return false;
+            } else {
+                //find the next item
+                $select = $this->select();
+                $select->order('position ASC');
+                $select->where("position > ?", $position);
+                $select->where("menu_id = ?", $row->menu_id);
+                $nextItem = $this->fetchRow($select);
+                if ($nextItem) {
+                    //switch positions with the next item
+                    $nextPosition = $nextItem->position;
+                    $nextItem->position = $position;
+                    $nextItem->save();
+                    $row->position = $nextPosition;
+                    $row->save();
+                }
+            }
+        } else {
+            throw new Zend_Exception("Error loading menu item");
+        }
+    }
+
+    public function updateItem($itemId, $label, $pageId = 0, $link = null) {
+        $row = $this->find($itemId)->current();
+        if ($row) {
+            $row->label = $label;
+            $row->page_id = $pageId;
+            if ($pageId < 1) {
+                $row->link = $link;
+            } else {
+                $row->link = null;
+            }
+            return $row->save();
+        } else {
+            throw new Zend_Exception("Error loading menu item");
+        }
+    }
+
+    public function deleteItem($itemId) {
+        $row = $this->find($itemId)->current();
+        if ($row) {
+            return $row->delete();
+        } else {
+            throw new Zend_Exception("Error loading menu item");
+        }
     }
 
 }
